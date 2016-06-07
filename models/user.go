1bad8cb67f13d316f392b67da0adcc71caefe9df
Go Takagi <takagi@shimastripe.com>
2016-06-07 20:41:31 +0900

    Change json field -> snake_case

--- models/user.go
+++ models/user.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	ID          uint `gorm:"primary_key"`
-	Name        string
-	AccountName string
-	Email       string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          uint      `gorm:"primary_key" json:"id"`
+	Name        string    `json:"name"`
+	AccountName string    `json:"account_name"`
+	Email       string    `json:"email"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
