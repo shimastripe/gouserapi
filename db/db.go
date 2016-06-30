0b09f8a3a0bf62ce5d7862f505bf33f3fc7691eb
Go Takagi <takagi@shimastripe.com>
2016-06-30 20:18:46 +0900

    Change from global variable to local variable

--- db/db.go
+++ db/db.go
@@ -11,14 +11,9 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-var (
-	DB  *gorm.DB
-	err error
-)
-
 func Connect(c *gin.Engine) *gorm.DB {
 	dir := filepath.Dir("db/database.db")
-	DB, err = gorm.Open("sqlite3", dir+"/database.db")
+	DB, err := gorm.Open("sqlite3", dir+"/database.db")
 	if err != nil {
 		log.Fatalf("Got error when connect database, the error is '%v'", err)
 	}
