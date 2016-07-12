0e7fb30c63d1aca26620513bcc48c9c600311793
Go Takagi <takagi@shimastripe.com>
2016-07-12 15:54:03 +0900

    Add case analysis db.Preload()

--- controllers/user.go
+++ controllers/user.go
@@ -2,24 +2,51 @@ package controllers
 
 import (
 	"net/http"
+	"strings"
 
-	"github.com/shimastripe/gouserapi/db"
+	dbpkg "github.com/shimastripe/gouserapi/db"
 	"github.com/shimastripe/gouserapi/models"
-	"github.com/shimastripe/gouserapi/pagination"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
+func setPreload(fields string, db *gorm.DB) ([]string, *gorm.DB) {
+	list := strings.Split(fields, ",")
+	sel := strings.Split(fields, ",")
+	offset := 0
+	for key, val := range list {
+		switch val {
+		case "profile":
+			db = db.Preload("Profile")
+			db = db.Preload("Profile.Nation")
+			sel = append(sel[:(key-offset)], sel[(key+1-offset):]...)
+			offset += 1
+		case "account_name":
+			db = db.Preload("AccountName")
+			sel = append(sel[:(key-offset)], sel[(key+1-offset):]...)
+			offset += 1
+		case "emails":
+			db = db.Preload("Emails")
+			sel = append(sel[:(key-offset)], sel[(key+1-offset):]...)
+			offset += 1
+		}
+	}
+	return sel, db
+}
+
 func GetUsers(c *gin.Context) {
-	pagination := pagination.Pagination{}
+	pagination := dbpkg.Pagination{}
 	db, err := pagination.Paginate(c)
 	if err != nil {
 		c.JSON(400, gin.H{"error": "invalid parameter."})
 		return
 	}
 	fields := c.DefaultQuery("fields", "*")
+	sel, db := setPreload(fields, db)
+
 	var users []models.User
-	err = db.Select(fields).Preload("Profile").Preload("Profile.Nation").Preload("AccountName").Preload("Emails").Find(&users).Error
+	err = db.Select(sel).Find(&users).Error
 	if err != nil {
 		c.JSON(500, gin.H{"error": "error occured"})
 		return
@@ -37,11 +64,12 @@ func GetUsers(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	db := db.DBInstance(c)
+	db := dbpkg.DBInstance(c)
 	id := c.Params.ByName("id")
 	fields := c.DefaultQuery("fields", "*")
+	sel, db := setPreload(fields, db)
 	var user models.User
-	err := db.Select(fields).Preload("Profile").Preload("Profile.Nation").Preload("AccountName").Preload("Emails").First(&user, id).Error
+	err := db.Select(sel).First(&user, id).Error
 	if err != nil {
 		content := gin.H{"error": "user with id#" + id + " not found"}
 		c.JSON(404, content)
@@ -52,7 +80,7 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	db := db.DBInstance(c)
+	db := dbpkg.DBInstance(c)
 	var user models.User
 	c.Bind(&user)
 	if db.Create(&user).Error != nil {
@@ -64,7 +92,7 @@ func CreateUser(c *gin.Context) {
 }
 
 func UpdateUser(c *gin.Context) {
-	db := db.DBInstance(c)
+	db := dbpkg.DBInstance(c)
 	id := c.Params.ByName("id")
 	var user models.User
 	if db.First(&user, id).Error != nil {
@@ -78,7 +106,7 @@ func UpdateUser(c *gin.Context) {
 }
 
 func DeleteUser(c *gin.Context) {
-	db := db.DBInstance(c)
+	db := dbpkg.DBInstance(c)
 	id := c.Params.ByName("id")
 	var user models.User
 	if db.First(&user, id).Error != nil {
